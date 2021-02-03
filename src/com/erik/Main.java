a32d7bb59dc04b3f94c54e18d8e4cce56fbb8f0f
erikwastaken <46598911+erikwastaken@users.noreply.github.com>
2021-02-03 16:59:13 +0100

    provide file containing puzzle on command line

--- src/com/erik/Main.java
+++ src/com/erik/Main.java
@@ -1,8 +1,23 @@
 package com.erik;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        if (args.length == 0) {
+            System.out.println("Error, provide a file path to the puzzle");
+            return;
+        }
+        try {
+            String input = Files.readString(Path.of(args[0]));
+            Solver s = new Solver(input);
+            System.out.println(s.solve());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
